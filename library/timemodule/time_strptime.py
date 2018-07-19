52484bf976ca7e10969bdd8d60e3ec4416baf596
zhangheng <zhangheng2683@gmail.com>
2018-07-20 00:09:43 +0800

    zh

--- library/timemodule/time_strptime.py
+++ library/timemodule/time_strptime.py
@@ -22,6 +22,7 @@ print('Now:', now)
 parsed = time.strptime(now)
 print('\nParsed:')
 show_struct(parsed)
+print(type(parsed))
 
 print('\nFormatted:',
       time.strftime('%Y-%m-%d %H:%M:%S'), parsed)
