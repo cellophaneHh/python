525d148ad72989d6dfc1c225341dfe84bfd848c8
zhangheng <zhangheng2683@gmail.com>
2018-07-26 07:15:29 +0800

    zh

--- library/subprocessmodule/subprocess_popen.read.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-print('read: ')
-proc = subprocess.Popen(
-    ['echo', '"to stdout"'],
-    stdout=subprocess.PIPE,
-)
-stdout_value = proc.communicate()[0].decode('utf-8')
-print('stdout: ', repr(stdout_value))
