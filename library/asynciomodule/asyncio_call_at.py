ecf09297866c378f511dd6d43e4892259ad70688
zhangheng <zhangheng2683@gmail.com>
2018-08-07 07:21:03 +0800

    zh

--- library/asynciomodule/asyncio_call_at.py
+++ library/asynciomodule/asyncio_call_at.py
@@ -19,7 +19,7 @@ async def main(loop):
     loop.call_at(now + 0.2, callback, 1, loop)
     loop.call_at(now + 0.1, callback, 2, loop)
 
-    await asyncio.sleep(1) # 阻塞1s
+    await asyncio.sleep(1)  # 阻塞1s
 
 
 event_loop = asyncio.get_event_loop()
