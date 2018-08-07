cf9878c171fde62c9dabf4342b7a5e36a54ed90e
zhangheng <zhangheng2683@gmail.com>
2018-08-07 23:57:55 +0800

    asyncio-future

---
+++ library/asynciomodule/asyncio_future_callback.py
@@ -0,0 +1,28 @@
+import asyncio
+import functools
+
+
+def callback(future, n):
+    print('{}: future done: {}'.format(n, future.result()))
+
+
+async def register_callbacks(all_done):
+    print('registering callbacks on future')
+    # future返回后进行回调
+    all_done.add_done_callback(functools.partial(callback, n=1))
+    all_done.add_done_callback(functools.partial(callback, n=2))
+
+
+async def main(all_done):
+    await register_callbacks(all_done)
+    print('setting result of future')
+    # 设置结果，future完结
+    all_done.set_result('the result')
+
+
+event_loop = asyncio.get_event_loop()
+try:
+    all_done = asyncio.Future()
+    event_loop.run_until_complete(main(all_done))
+finally:
+    event_loop.close()
---
+++ library/asynciomodule/asyncio_future_event_loop.py
@@ -0,0 +1,25 @@
+import asyncio
+import time
+
+
+def mark_done(future, result):
+    time.sleep(2)
+    print('setting future result to {!r}'.format(result))
+    future.set_result(result)
+
+
+event_loop = asyncio.get_event_loop()
+try:
+    all_done = asyncio.Future()
+
+    print('scheduling mark_done')
+    event_loop.call_soon(mark_done, all_done, 'the result')
+
+    print('entering event loop')
+    result = event_loop.run_until_complete(all_done)
+    print('returned result: {!r}'.format(result))
+finally:
+    print('closing event loop')
+    event_loop.close()
+
+print('future result: {!r}'.format(all_done.result()))
